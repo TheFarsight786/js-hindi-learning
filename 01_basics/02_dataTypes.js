9942b789b4bc9ed9ccbe186360b0619a8e9b5b93
TheFarsight786 <thefarsight786@gmail.com>
2024-09-17 19:39:03 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+//alert( 3 +3 )
+
+let score = undefined
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+const heroes = ["ironman" , "thor" ,"loki"]
+const myobject = {
+    health: 0 ,
+    age: 12 , 
+    name : abhay
+}
+
+const myfunc = Function(any) {
+    console.log("hello");
+    
+}
